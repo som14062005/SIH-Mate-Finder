4049c0f59c5c750793d69ca5eee775b4a139a673
som14062005 <praveensomasundaram2005@gmail.com>
2025-08-26 23:01:15 +0530

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900 text-white text-3xl font-bold">
+      Hello Tailwind + React 🚀
+    </div>
+  );
+}
+
+export default App;
